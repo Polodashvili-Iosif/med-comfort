febec8b83df1eaf724da510a1dde5dfef530f2fa
Polodashvili-Iosif <iosif.polodashvili@mail.ru>
2023-05-28 19:20:24 +0300

    База данных изменена на PostgreSQL

--- med_comfort/med_comfort/settings.py
+++ med_comfort/med_comfort/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'med_comfort.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
